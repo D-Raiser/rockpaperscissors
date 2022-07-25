943fab165af6d609d2ece1e9cfff462b96bca2b4
D-Raiser <raiserdaniel@googlemail.com>
2022-07-25 18:48:27 +0200

    Tests for play()

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,23 @@
+enum class Result {
+    WIN, DRAW, LOSS
+}
+
+enum class Action {
+    ROCK,
+    PAPER,
+    SCISSOR
+}
+
+// run `mvn exec:java`
 fun main(args: Array<String>) {
     println("Hello World!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+}
+
+// retuns the result of playing `action1` vs `action2` from the perspective of `action1`
+fun play( action1: Action, action2: Action) : Result {
+    TODO()
 }
---
+++ src/test/kotlin/MainTest.kt
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
+
+// run `mvn test`
+class MainTest {
+    @TestFactory
+    fun testPlay() = listOf(
+        Pair(Action.PAPER, Action.PAPER) to Result.DRAW,
+        Pair(Action.PAPER, Action.ROCK) to Result.WIN,
+        Pair(Action.PAPER, Action.SCISSOR) to Result.LOSS,
+        Pair(Action.ROCK, Action.PAPER) to Result.LOSS,
+        Pair(Action.ROCK, Action.ROCK) to Result.DRAW,
+        Pair(Action.ROCK, Action.SCISSOR) to Result.WIN,
+        Pair(Action.SCISSOR, Action.PAPER) to Result.WIN,
+        Pair(Action.SCISSOR, Action.ROCK) to Result.LOSS,
+        Pair(Action.SCISSOR, Action.SCISSOR) to Result.DRAW,
+    ).map { (input, expected) -> DynamicTest.dynamicTest("${input.first} vs ${input.second} should produce $expected") {
+        Assertions.assertEquals(expected, play(input.first, input.second))
+    } }
+}
