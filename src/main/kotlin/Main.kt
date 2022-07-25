5735967f64455a50131f67b6f62e62e4af6b441f
D-Raiser <raiserdaniel@googlemail.com>
2022-07-25 19:53:22 +0200

    run game

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,6 @@
+import java.time.Clock
+import kotlin.random.Random
+
 enum class Result {
     WIN, DRAW, LOSS
 }
@@ -8,8 +11,29 @@ enum class Action {
     SCISSOR
 }
 
-// run `mvn exec:java`
-fun main() {}
+const val games = 100
+
+// run `mvn clean compile exec:java`
+fun main() {
+    val player1Strategy = RandomStrategy(Random(Clock.systemUTC().millis()))
+    val player2Strategy = StaticStrategy(Action.ROCK)
+
+    var player1wins = 0
+    var player2wins = 0
+    for(i in 1..games) {
+        when(play(player1Strategy, player2Strategy)) {
+            Result.WIN -> player1wins++
+            Result.LOSS -> player2wins++
+        }
+    }
+
+    println("Played $games games")
+    println("Player 1 wins: $player1wins")
+    println("Player 2 wins: $player2wins")
+    println("Draws: ${games-player1wins-player2wins}")
+}
+
+fun play(strategy1: Strategy, strategy2: Strategy) : Result = play(strategy1.getNextAction(), strategy2.getNextAction())
 
 // returns the result of playing `action1` vs `action2` from the perspective of `action1`
 fun play(action1: Action, action2: Action): Result {
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -2,10 +2,9 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.TestFactory
 
-// run `mvn test`
+// run `mvn clean test`
 class MainTest {
-    @TestFactory
-    fun testPlay() = listOf(
+    private val cases = listOf(
         Pair(Action.PAPER, Action.PAPER) to Result.DRAW,
         Pair(Action.PAPER, Action.ROCK) to Result.WIN,
         Pair(Action.PAPER, Action.SCISSOR) to Result.LOSS,
@@ -15,9 +14,20 @@ class MainTest {
         Pair(Action.SCISSOR, Action.PAPER) to Result.WIN,
         Pair(Action.SCISSOR, Action.ROCK) to Result.LOSS,
         Pair(Action.SCISSOR, Action.SCISSOR) to Result.DRAW,
-    ).map { (input, expected) ->
+    )
+
+    @TestFactory
+    fun testPlay() = cases.map { (input, expected) ->
         DynamicTest.dynamicTest("${input.first} vs ${input.second} should produce $expected") {
             Assertions.assertEquals(expected, play(input.first, input.second))
+        }
+    }
+
+    @TestFactory
+    // testing the overloaded variant of `play()` the same way the original was tested via `StaticStrategy`
+    fun testPlayStrategies() = cases.map { (input, expected) ->
+        DynamicTest.dynamicTest("${input.first} vs ${input.second} should produce $expected") {
+            Assertions.assertEquals(expected, play(StaticStrategy(input.first), StaticStrategy(input.second)))
         }
     }
 }
